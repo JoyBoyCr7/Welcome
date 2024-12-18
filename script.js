e1782cb4a4f8414ea3a5a419fcbc10cd0fcbe717
JoyBoyCr7 <Rnyongkah@gmail.com>
2024-12-17 22:30:54 -0600

    jaja

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 
-function test(){
-    console.log("funk")
+// function test(){
+//     console.log("funk")
+// }
+
+// setTimeout(test,200)
+
+async function randomCats(){
+    const cats = await fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+    const allcats = await cats.json()
+    // console.log(allcats)
+    for (let cat of allcats){
+        const catimg = document.createElement('img')
+        catimg.setAttribute("src",`${cat.url}`)
+        document.body.append(catimg)
+        
+    }
 }
+// document.getElementById("opening").innerHTML = "yooyoo"
+// for (const step = 0; step < length(allcats); step++){
+//     console.log
+// }
 
-setTimeout(test,200)
-
-async function randomDog(){
-    const dog = await fetch("https://dog.ceo/api/breeds/image/random")
-    const alldogs = await dog.json()
-    console.log(alldogs)
-}
-
-document.getElementById("opening").innerHTML = "yooyoo"
-
-randomDog()
+// document.getElementById("cats").innerHTML = "yoo"
+randomCats()
 
